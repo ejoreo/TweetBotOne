47d46b3bea086d8590b0eaf3e3c30ea3dd012593
Eric O'Rear <ejorear@gmail.com>
2016-07-18 13:01:49 -0400

    Database migration and twitter client config

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160718164757) do
+
+  create_table "users", force: :cascade do |t|
+    t.string   "provide"
+    t.string   "uid"
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
