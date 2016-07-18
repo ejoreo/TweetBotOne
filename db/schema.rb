30b36d03b6378d2bdb1303af25be6259878da433
Eric O'Rear <ejorear@gmail.com>
2016-07-18 13:53:19 -0400

    Response seed update.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160718164757) do
+ActiveRecord::Schema.define(version: 20160718173043) do
+
+  create_table "bots", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "responses", force: :cascade do |t|
+    t.string   "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "provide"
