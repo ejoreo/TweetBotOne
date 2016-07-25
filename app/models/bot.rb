eceb73685fb11f7db95f8baa29714dcb0ed1e2b7
Eric O'Rear <ejorear@gmail.com>
2016-07-25 18:58:58 -0400

    Reconfigured the bot. Defined "dig for tweets" and "timeline grab" methods. Timeline Grab collects 20 most recent tweets according to our parameters (no retweets, no replies, url-containing).

--- app/models/bot.rb
+++ app/models/bot.rb
@@ -5,23 +5,29 @@ class Bot < ApplicationRecord
     CLIENT.search(words, lang: "en").first.text
   end
 
-  def self.f_u number, words #f_u = find_user
-    CLIENT.search(words, lang: "en").take(number).each { |t|
-      url  = nil
+  def self.dig_for_tweets tweets
+    tweets.each { |t|
+      url = nil
       if t.attrs[:entities][:urls].size > 0
         url = t.attrs[:entities][:urls][0][:expanded_url]
-
-        #TweetInfo.create(name: t.user.screen_name, tweet_id: t.id.to_s, link: t.url, text: t.full_text)
       end
-      @tweet_deet = TweetDeet.create(user: t.user.screen_name,
-                                  tweetid: t.id.to_s,
-                                  link: url,
-                                  tweet: t.full_text)
 
+      @tweet_deet = TweetDeet.create(user: t.user.screen_name,
+                                     tweetid: t.id.to_s,
+                                     link: url,
+                                     tweet: t.full_text)
       puts @tweet_deet.inspect
-      @tweet_deet.save
     }
   end
 
+  def self.timeline_grab screen_name
+    tweet_array = CLIENT.user_timeline(screen_name, {include_rts: false,
+                                                     exclude_replies: true})
+    self.dig_for_tweets tweet_array
+  end
+
+  #def self.find_user username
+    #CLIENT.user_timeline(username).text
+  #end
 end
 #[:entities][:user_mentions][1]
